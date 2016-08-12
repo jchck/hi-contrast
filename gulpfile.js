ba375c2612928f269173bacd8fa955149b0e3839
jchck <hey@justinchick.com>
2016-08-12 10:44:58 -0500

    use browserSync to watch and reload browser on sass change

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@
 
 var gulp	= require('gulp');
 var sass	= require('gulp-sass');
-var maps	= require('gulp-sourcemaps')
+var maps	= require('gulp-sourcemaps');
+var sync	= require('browser-sync').create();
+var reload	= sync.reloaded;
 
 gulp.task('sass', function() {
 	return gulp.src('./assets/sass/main.scss')
@@ -17,4 +19,17 @@ gulp.task('sass', function() {
 		.pipe(maps.write('./'))
 
 		.pipe(gulp.dest('./assets/css'))
-})
+
+		.pipe(sync.stream())
+});
+
+gulp.task('watch', function(){
+
+	sync.init({
+		proxy : 'http://localhost:2368/'
+	});
+
+	gulp.watch('./assets/sass/**/*', ['sass']);
+});
+
+gulp.task('default', ['watch']);
