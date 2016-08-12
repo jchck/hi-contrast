d145b149c98c1991b4650daef8831ec4804ec698
jchck <hey@justinchick.com>
2016-08-12 00:45:41 -0500

    sass processing via gulp

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var gulp	= require('gulp');
+var sass	= require('gulp-sass');
+var maps	= require('gulp-sourcemaps')
+
+gulp.task('sass', function() {
+	return gulp.src('./assets/sass/main.scss')
+
+		.pipe(maps.init())
+
+		.pipe(sass.sync({
+			outputStyle: 'compressed',
+			sourceMap: true
+		}).on('error', sass.logError))
+
+		.pipe(maps.write('./assets/css'))
+
+		.pipe(gulp.dest('./'))
+})
